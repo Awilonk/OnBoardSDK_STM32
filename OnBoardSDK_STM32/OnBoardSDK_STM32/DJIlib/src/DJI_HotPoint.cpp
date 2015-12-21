734f56dd966ba13be472fe32143718fc1c42c77f
Awilonk <jjkkty@qq.com>
2015-12-21 15:38:17 +0800

    no message

---
+++ OnBoardSDK_STM32/OnBoardSDK_STM32/DJIlib/src/DJI_HotPoint.cpp
@@ -0,0 +1,190 @@
+#include "DJI_HotPoint.h"
+#include <string.h>
+
+using namespace DJI::onboardSDK;
+
+HotPoint::HotPoint(CoreAPI *ControlAPI)
+{
+    api = ControlAPI;
+
+    initData();
+}
+
+void HotPoint::initData()
+{
+    data.version = 0;
+
+    data.altitude = api->getBroadcastData().pos.altitude;
+    data.longtitude = api->getBroadcastData().pos.longtitude;
+    data.latitude = api->getBroadcastData().pos.latitude;
+
+    data.radius = 10;
+    data.palstance = 15;
+    data.clockwise = 1;
+    data.startPoint = HotPoint::VIEW_NEARBY;
+    data.yawMode = HotPoint::YAW_INSIDE;
+}
+
+void HotPoint::start(CallBack callback)
+{
+//    api->send(2, 1, SET_MISSION, CODE_HOTPOINT_START, &data, sizeof(data),
+//              callback ? callback : startCallback, 500, 2);
+		   api->send(2, 0, SET_MISSION, CODE_HOTPOINT_START, &data, sizeof(data),
+              callback ? callback : startCallback, 500, 2);
+			 //UNENCRYPTED
+		
+}
+
+void HotPoint::stop(CallBack callback)
+{
+    uint8_t zero = 0;
+//    api->send(2, 1, SET_MISSION, CODE_HOTPOINT_STOP, &zero, sizeof(zero),
+//              callback ? callback : commonCallback, 500, 2);
+    api->send(2, 0, SET_MISSION, CODE_HOTPOINT_STOP, &zero, sizeof(zero),
+              callback ? callback : commonCallback, 500, 2);
+		//UNENCRYPTED
+}
+
+void HotPoint::resetPalstance(HotPoint::Palstance &Data, CallBack callback)
+{
+    data.palstance = Data.palstance;
+    data.clockwise = Data.clockwise ? 1 : 0;
+//    api->send(2, 1, SET_MISSION, CODE_HOTPOINT_PALSTANCE, &Data, sizeof(Data),
+//              callback ? callback : commonCallback, 500, 2);
+		 api->send(2, 0, SET_MISSION, CODE_HOTPOINT_PALSTANCE, &Data, sizeof(Data),
+              callback ? callback : commonCallback, 500, 2);
+		 //UNENCRYPTED
+}
+
+void HotPoint::resetPalstance(float32_t palstance, bool isClockwise,
+                              CallBack callback)
+{
+    Palstance p;
+    p.palstance = palstance;
+    p.clockwise = isClockwise ? 1 : 0;
+    resetPalstance(p, callback);
+}
+
+void HotPoint::resetRadius(float32_t degree, CallBack callback,UserData userData)
+{
+	//UNENCRYPTED
+//    api->send(2, 1, SET_MISSION, CODE_HOTPOINT_RADIUS, &degree, sizeof(degree), 500, 2,
+//              callback ? callback : commonCallback,userData);
+		api->send(2, 0, SET_MISSION, CODE_HOTPOINT_RADIUS, &degree, sizeof(degree), 500, 2,
+              callback ? callback : commonCallback,userData);
+}
+
+void HotPoint::resetYaw(CallBack callback, UserData userData)
+{
+    uint8_t zero = 0;
+    api->send(2, 0, SET_MISSION, CODE_HOTPOINT_SETYAW, &zero, sizeof(zero), 500,
+              2, callback ? callback : commonCallback, userData);
+//		   api->send(2, 1, SET_MISSION, CODE_HOTPOINT_SETYAW, &zero, sizeof(zero), 500,
+//              2, callback ? callback : commonCallback, userData);
+		//UNENCRYPTED
+}
+
+void HotPoint::readData(CallBack callback, UserData userData)
+{
+    uint8_t zero = 0;
+    api->send(2, 0, SET_MISSION, CODE_HOTPOINT_LOAD, &zero, sizeof(zero), 500,
+              2, callback ? callback : commonCallback, userData);
+//		  api->send(2, 1, SET_MISSION, CODE_HOTPOINT_LOAD, &zero, sizeof(zero), 500,
+//              2, callback ? callback : commonCallback, userData);
+		//UNENCRYPTED
+}
+
+void HotPoint::setData(const HotPointData &value) { data = value; }
+
+void HotPoint::setHotPoint(float64_t longtitude, float64_t latitude,
+                           float64_t altitude)
+{
+    data.longtitude = longtitude;
+    data.latitude = latitude;
+    data.altitude = altitude;
+}
+
+void HotPoint::setHotPoint(GPSData gps)
+{
+    data.longtitude = gps.longtitude;
+    data.latitude = gps.latitude;
+    data.altitude = gps.altitude;
+}
+
+void HotPoint::setRadius(float64_t meter) { data.radius = meter; }
+
+void HotPoint::setPalstance(float32_t defree) { data.palstance = defree; }  //what's the units?
+
+void HotPoint::setClockwise(bool isClockwise)
+{
+    data.clockwise = isClockwise ? 1 : 0;
+}
+
+void HotPoint::setCameraView(HotPoint::View view) { data.startPoint = view; }
+
+void HotPoint::setYawMode(HotPoint::YawMode mode) { data.yawMode = mode; }
+
+HotPointData HotPoint::getData() const { return data; }
+
+void HotPoint::startCallback(CoreAPI *This, Header *header,
+                             UserData userdata __UNUSED)
+{
+    StartACK ack;
+    if (header->length - EXC_DATA_SIZE <= sizeof(StartACK))
+    {
+        memcpy((unsigned char *)&ack, (unsigned char *)header + sizeof(Header),
+               (header->length - EXC_DATA_SIZE));
+        API_LOG(This->getDriver(), STATUS_LOG,
+                "Start ACK has Max radius %f, ack 0x%X", ack.maxRadius,
+                ack.ack);
+        if (!This->decodeMissionStatus(ack.ack))
+            API_LOG(This->getDriver(), ERROR_LOG, "decode ack error 0x%X",
+                    ack.ack);
+    }
+    else
+    {
+        API_LOG(This->getDriver(), ERROR_LOG,
+                "ACK is exception,seesion id %d,sequence %d\n",
+                header->sessionID, header->sequence_number);
+    }
+}
+
+void HotPoint::commonCallback(CoreAPI *This, Header *header,
+                              UserData userdata __UNUSED)
+{
+    API_LOG(This->getDriver(), STATUS_LOG, "HotPoint common");
+    CommonACK ack;
+    if (header->length - EXC_DATA_SIZE <= sizeof(ack))
+    {
+        memcpy((unsigned char *)&ack, (unsigned char *)header + sizeof(Header),
+               (header->length - EXC_DATA_SIZE));
+        if (!This->decodeMissionStatus(ack))
+            API_LOG(This->getDriver(), ERROR_LOG, "decode ack error 0x%X", ack);
+    }
+    else
+    {
+        API_LOG(This->getDriver(), ERROR_LOG,
+                "ACK is exception,seesion id %d,sequence %d\n",
+                header->sessionID, header->sequence_number);
+    }
+}
+
+void HotPoint::readCallback(CoreAPI *This, Header *header,
+                            UserData userdata __UNUSED)
+{
+    API_LOG(This->getDriver(), STATUS_LOG, "HotPoint common");
+    CommonACK ack;
+    if (header->length - EXC_DATA_SIZE <= sizeof(ack))
+    {
+        memcpy((unsigned char *)&ack, (unsigned char *)header + sizeof(Header),
+               (header->length - EXC_DATA_SIZE));
+        if (!This->decodeMissionStatus(ack))
+            API_LOG(This->getDriver(), ERROR_LOG, "decode ack error 0x%X", ack);
+    }
+    else
+    {
+        API_LOG(This->getDriver(), ERROR_LOG,
+                "ACK is exception,seesion id %d,sequence %d\n",
+                header->sessionID, header->sequence_number);
+    }
+}
---
+++ OnBoardSDK_STM32/OnBoardSDK_STM32/User/main.cpp
@@ -0,0 +1,71 @@
+/**
+  ******************************************************************************
+	*                                                                                            
+	*          --------               --------                 --------                                                       
+	*         |        |   USART2    |        |    USART3     |        |                                     
+	*         |   PC   | <---------> | stm32  |  <----------> |  M100  |                                                  
+	*         |        | (USB-TTL)   |        |               |        |                                    
+	*         |        |             |        |               |        |                                    
+	*          --------               --------                 --------                                                       
+	*                                                                                            
+	*                                                                                                                                                                                                                                                                
+	******************************************************************************  
+  */ 
+
+#include "main.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif //__cplusplus
+	
+/*-----------------------DJI_LIB VARIABLE-----------------------------*/
+using namespace DJI::onboardSDK;
+DJI::onboardSDK::HardDriver* driver = new STM32F4;
+CoreAPI defaultAPI = CoreAPI(driver);
+CoreAPI *coreApi = &defaultAPI; 
+Flight flight = Flight(coreApi);
+FlightData flightData ;
+VirtualRC virtualrc = VirtualRC(coreApi); 
+VirtualRCData myVRCdata={1024,1024,1024,1024,1024,1024,1024,1024,1024,1024,1024,1024,1024,1024,1024};
+
+/*----------------------USART VARIABLE------------------------------*/
+
+
+int main()
+{ 
+	BSPinit();
+ 	delay_nms(30);
+	printf("Initializing...\r\n");
+	
+	delay_nms(1000);
+	coreApi->getVersion();
+	delay_nms(20);
+	printf("Done~!\r\n");
+	
+ 	while(1)
+	{
+			if(Rx_Handle_Flag == 1)
+				{
+						Rx_Handle_Flag = 0;
+						Rx_buff_Handler();
+				}
+		delay_nms(50);
+	}
+}
+
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
+
+
+void delay_nms(u16 time)
+{    
+   u32 i=0;  
+   while(time--)
+   {
+      i=30000;  
+      while(i--) ;    
+   }
+}
+ 
